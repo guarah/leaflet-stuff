94116edc996d0b0095780f5aa5b47df602bbf1d8
Guaratã Alencar <guara_st@hotmail.com>
2017-09-14 00:47:03 -0300

    loading data from kml and kmz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import * as L from 'leaflet';
+import * as omnivore from '@mapbox/leaflet-omnivore';
+import extract from 'extract-zip';
+import JSZip from 'jszip/dist/jszip';
+import JSZipUtils from 'jszip-utils/dist/jszip-utils';
 
 @Component({
   selector: 'app-root',
@@ -29,5 +33,38 @@ export class AppComponent {
       'Big Circle': L.circle([ 46.95, -122 ], { radius: 5000 }),
       'Big Square': L.polygon([[ 46.8, -121.55 ], [ 46.9, -121.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]])
     }
+  }
+
+  public onMapReady(map: L.Map) {
+
+  JSZipUtils.getBinaryContent('./assets/z.kmz', function(err, data) {
+    if (err) {
+      alert(err);
+      return;
+    }
+    try {
+      JSZip.loadAsync(data)
+      .then(function(z) {
+        return z.file(/.*\.kml/)[0].async('string')
+      })
+      .then(function success(text) {
+        const p = omnivore.kml.parse(text).addTo(map);
+        map.fitBounds(p.getBounds());
+        p.eachLayer(function(layer){
+          layer.bindTooltip('<div style="width:300px">' + layer.feature.properties.description + '</div>');
+        });
+      },
+      function error(e) {
+        alert(e);
+      });
+    } catch (e) {
+      alert(e);
+    }
+  });
+
+
+    // const a = omnivore.kml('./assets/s.kml').addTo(map);
+    // console.log(`a`, a);
+    // map.fitBounds(a.getBounds());
   }
 }
